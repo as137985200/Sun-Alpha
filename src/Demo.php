4a36fc70c67e4308eafd98eefd857f404501e2c3
as137985200 <435219154@qq.com>
2023-03-02 10:17:12 +0800

    1.

---
+++ src/Demo.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Author：Sun-Alpha <as137985200@outlook.com>
+ */
+
+namespace sun\alpha;
+
+
+class Demo
+{
+    public static function sayHello()
+    {
+        return 'Good morning ';
+    }
+}
